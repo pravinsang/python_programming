6afc806758f9f7f9b3d5edcfb621c189b43337b8
Pravin Sangroula <pravin@ioepc.edu.np>
2024-05-23 15:40:48 +0545

    third

---
+++ arithmeticop.py
@@ -0,0 +1,20 @@
+a = 10
+b = 6
+
+# Perform arithmetic operations
+addition_result = a + b
+subtraction_result = a - b
+multiplication_result = a * b
+division_result = a / b
+floor_division_result = a // b
+modulus_result = a % b
+exponentiation_result = a ** b
+
+# Print results
+print("Addition:", addition_result)
+print("Subtraction:", subtraction_result)
+print("Multiplication:", multiplication_result)
+print("Division:", division_result)
+print("Floor Division:", floor_division_result)
+print("Modulus:", modulus_result)
+print("Exponentiation:", exponentiation_result)
