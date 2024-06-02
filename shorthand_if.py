6316b09160333314205e8381ff18d991edfda6f6
Pravin Sangroula <pravin@ioepc.edu.np>
2024-06-02 06:41:13 +0545

    Sixth commit

---
+++ shorthand_if.py
@@ -0,0 +1,9 @@
+# short hand if 
+# the if statement can be simplified into one line
+number = 10
+if number>0: print('Positive')
+
+#ternary operator if .. else
+percentage = 80
+result = "Distinction" if percentage >= 80 else "Not Distinction"
+print(result)
